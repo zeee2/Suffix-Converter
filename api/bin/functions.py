5200dbfba76570da6c07a8ae609726c29c92bc15
Nerina <64479867+Nerina1241@users.noreply.github.com>
2020-10-26 15:39:16 +0900

    웹사이트 / API서버
    yeeeeeeeeeeeeee

---
+++ api/bin/functions.py
@@ -0,0 +1,31 @@
+import re
+
+def replaceRight(original, old, new, count_right):
+    repeat=0
+    text = original
+    old_len = len(old)
+    
+    count_find = original.count(old)
+    if count_right > count_find :
+        repeat = count_find
+    else :
+        repeat = count_right
+
+    while(repeat):
+      find_index = text.rfind(old)
+      text = text[:find_index] + new + text[find_index+old_len:]
+
+      repeat -= 1
+      
+    return text
+
+def convert_text(txt, before, after):
+    data = []
+    for part in re.split('[.,]\s+', txt):
+        last = part[:2]
+        part = replaceRight(part, before, after, 1)
+
+        data.append(part)
+
+        print(f"----------------------\n{last}\n--------------------")
+    return {'result': data}
---
+++ api/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from sqlalchemy import create_engine
+import json
+from flask_jsonpify import jsonify
+from bin.functions import *
+import pymysql
+
+import random
+
+app = Flask('Suffix-Converter')
+api = Api(app)
+
+class mainclass(Resource):
+    def get(self):
+        json = {"status": {"code": '200',"message": "ok"}}
+        return json
+
+class Converter(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('txt', required=True, type=str, help="")
+        parser.add_argument('before', required=True, type=str, help="")
+        parser.add_argument('after', required=True, type=str, help="")
+        args = parser.parse_args() 
+
+        txt = args['txt']
+        before = args['before']
+        after = args['after']
+
+        result = convert_text(txt, before, after)
+
+        return result
+
+
+api.add_resource(mainclass, '/')
+api.add_resource(Converter, '/api/v1')
+
+if __name__ == '__main__':
+    app.run(port=1234, host="127.0.0.1", debug=True)
